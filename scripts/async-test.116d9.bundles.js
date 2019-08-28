6206e1dbc2d414bee2dde3f3e39366a7ae083bf4
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-08-28 14:34:42 +0000

    Deploy JanssenZhang/JanssenZhang.github.io to github.com/JanssenZhang/JanssenZhang.github.io.git:master

--- scripts/async-test.116d9.bundles.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[0],{4:function(o,n,s){"use strict";s.r(n);var c=s(0);console.log("async引用",c.a.version);const e={init(){console.log("test")}};n.default=e}}]);
